58262079de2e6c8a07e327049e61b3365404516c
Xavier Smet <xaviersmet@gmail.com>
2015-09-04 08:00:42 +0000

    Cleanup & integration test flash messages

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -22,5 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert          flash.key?("success") # We should get a "succes" flash message
+    assert_not      flash.key?("danger")  # ... and definitely no "danger" flash
   end
 end
