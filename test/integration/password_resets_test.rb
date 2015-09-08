b7fc821a2c500cb25226016d4c136e12c47daec4
Xavier Smet <xaviersmet@gmail.com>
2015-09-08 15:16:26 +0000

    Test expired password reset token

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -5,6 +5,22 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     ActionMailer::Base.deliveries.clear
     @user = users(:xavier)
   end
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, password_reset: { email: @user.email }
+    
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+            email: @user.email,
+            user: { password: "foobar", password_confirmation: "foobar" }
+    
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
+  end
+    
   
   test "password resets" do
     get new_password_reset_path
