f8ef8b26ac978cb48cabc21a99f1c1a02fae3f97
Xavier Smet <xaviersmet@gmail.com>
2015-09-09 09:04:16 +0000

    Error messages now work for Users and Microposts + micropost form for logged in users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # @micropost won't be defined on the homepage if nobody is logged in
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
