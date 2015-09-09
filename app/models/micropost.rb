41a56995886a695ec6042b2c5cc13c451530ff42
Xavier Smet <xaviersmet@gmail.com>
2015-09-09 10:13:01 +0000

    Define a .picture attribute on Micropost with CarrierWave gem uploader

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) } # Most recent first
+
+  # Tell CarrierWave to associate the uploader with the Micropost.picture attribute
+  mount_uploader :picture, PictureUploader
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
