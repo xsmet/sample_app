eecbd6713f27630bc40f4a69d37c100a26aa411a
Xavier Smet <xaviersmet@gmail.com>
2015-09-09 14:07:14 +0000

    S3 authorization mechanism fix?

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,19 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :provider              => 'AWS',                  #required
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],   #required
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],   #required
+      :region                => 'eu-central-1',         #optional, default 'us-east-1'
+      :s3_signature_version  => :v4
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['S3_BUCKET']        #required
   end
 end
+
+#   :host                   => 's3.example.com',             #optional, default nil
+#   :endpoint               => 'https://s3.example.com:8080' #optional, default nil
+# config.root = Rails.root.join('tmp') # adding these...
+# config.cache_dir = 'carrierwave' # ...two lines
+# config.fog_public     = false                                   #optional, default true
+# config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  #optional, default {}
